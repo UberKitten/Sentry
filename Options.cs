7d08c568daaf208628e550f7bcabbc4863b55e77
Michael West <michael@mwe.st>
2018-01-31 02:55:02 -0600

    Add hidden quorum delay options

--- Options.cs
+++ Options.cs
@@ -19,6 +19,20 @@ namespace Sentry
 
         [Option('t', Default = 600, HelpText = "How long, in seconds, to wait between each check.")]
         public int LoopDelay { get; set; }
+
+        [Option(Default = true, HelpText = "Whether to check for other Sentries when attempting to run an action.")]
+        public bool CheckQuorum { get; set; }
+
+        // Modify the below settings at your own risk!
+
+        [Option(Default = 1.25, Hidden = true, HelpText = "Multiplied by LoopDelay to determine the minimum time to wait before assuming quorum success.")]
+        public double QuorumCheckDelayMultiplier { get; set; }
+
+        [Option(Default = 30, Hidden = true, HelpText = "Minimum number of seconds for the random delay when checking for quorum")]
+        public int QuorumJitterLowerBound { get; set; }
+
+        [Option(Default = 60, Hidden = true, HelpText = "Maximum number of seconds for the random delay when checking for quorum")]
+        public int QuorumJitterUpperBound { get; set; }
     }
 
     [Verb("makeexample", HelpText = "Create example config.json file")]
