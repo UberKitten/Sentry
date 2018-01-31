0ff93a16e0a4896a6dd49ccdf160352933964c21
Michael West <michael@mwe.st>
2018-01-31 01:30:20 -0600

    Add command line options and logging

--- Options.cs
+++ Options.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,24 @@ namespace Sentry
 {
     class Options
     {
+        [Option('c', Default = "config.json", HelpText = "Configuration file in JSON format to use.")]
+        public string ConfigFile { get; set; }
+    }
+
+    [Verb("run", HelpText = "Normal Sentry operation mode")]
+    class RunOptions : Options
+    {
+        [Option(Default = false, HelpText = "Whether to loop through checks a single time before stopping.")]
+        public bool LoopOnce { get; set; }
+
+        [Option('t', Default = 600, HelpText = "How long, in seconds, to wait between each check.")]
+        public int LoopDelay { get; set; }
+    }
+
+    [Verb("makeexample", HelpText = "Create example config.json file")]
+    class ExampleConfigOptions : Options
+    {
+        [Option('o', Default = true, HelpText = "Whether to overwrite the existing config file.")]
+        public bool Overwrite { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using CommandLine;
+using Newtonsoft.Json;
+using NLog;
 using System;
 using System.IO;
 
@@ -6,11 +8,48 @@ namespace Sentry
 {
     class Program
     {
+        private static Logger logger = LogManager.GetLogger("Sentry");
+
         static void Main(string[] args)
         {
+
+            var options = Parser.Default.ParseArguments<RunOptions, ExampleConfigOptions>(args)
+                .WithParsed<RunOptions>(opts => Run(opts))
+                .WithParsed<ExampleConfigOptions>(opts => ExampleConfig(opts));
+            
+        }
+
+        private static void ExampleConfig(ExampleConfigOptions options)
+        {
+            logger.Info("Generating example config file {0}", options.ConfigFile);
+
             var config = Config.GetExample();
+
             var json = JsonConvert.SerializeObject(config, Formatting.Indented);
-            File.WriteAllText("config.json", json);
+            logger.Trace("Example JSON:");
+            logger.Trace(json);
+
+            if (options.Overwrite || !File.Exists(options.ConfigFile))
+            {
+                try
+                {
+                    File.WriteAllText(options.ConfigFile, json);
+                    logger.Info("Example config file written");
+                }
+                catch (IOException ex)
+                {
+                    logger.Error(ex, "Error writing example config file");
+                }
+            }
+            else
+            {
+                logger.Error("Not overwriting {0}", options.ConfigFile);
+            }
+        }
+
+        private static void Run(RunOptions options)
+        {
+
         }
     }
 }
