292b0a27051b0a2220e012e6be1bff92a15263b1
UberKitten <566438+UberKitten@users.noreply.github.com>
2018-02-06 06:04:20 -0600

    Add RestSharp and DynamicJsonDeserializer

---
+++ DynamicJsonDeserializer.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sentry
+{
+    // https://gist.github.com/rdingwall/1884642
+    public class DynamicJsonDeserializer : IDeserializer
+    {
+        public string RootElement { get; set; }
+        public string Namespace { get; set; }
+        public string DateFormat { get; set; }
+
+        public T Deserialize<T>(IRestResponse response)
+        {
+            return JsonConvert.DeserializeObject<dynamic>(response.Content);
+        }
+    }
+}
