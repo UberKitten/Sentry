1dc6f97da32817185788710691a2d95640242084
Michael West <michael@mwe.st>
2018-02-06 06:06:21 -0600

    Add Twitter API implementation with working check

--- Services/Twitter.cs
+++ Services/Twitter.cs
@@ -1,14 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RestSharp;
+using RestSharp.Authenticators;
 using Sentry.Config;
 
 namespace Sentry.Services
 {
     class Twitter : BaseService
     {
+        protected class ServiceOptions {
+            public string ConsumerKey { get; set; }
+            public string ConsumerSecret { get; set; }
+            public string Token { get; set; }
+            public string TokenSecret { get; set; }
+        };
+
+        protected ServiceOptions Options { get; set; }
+
+        protected RestClient client = new RestClient("https://api.twitter.com/");
+        protected long user_id = -1;
+
         public Twitter(ServiceConfig config) : base(config)
         {
+            Options = InitializeOptions<ServiceOptions>();
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(Options.ConsumerKey, Options.ConsumerSecret, Options.Token, Options.TokenSecret);
+            client.AddHandler("application/json", new DynamicJsonDeserializer());
         }
+
+        private IRestResponse<dynamic> VerifyCredentials()
+        {
+            var request = new RestRequest("/1.1/account/verify_credentials.json", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader("Content-Type", "application/json; charset=utf-8");
+
+            IRestResponse<dynamic> response = client.Execute<dynamic>(request);
+            logger.Trace("VerifyCredentials response: {0}", response.Content);
+            return response;
+        }
+
+        private IRestResponse<dynamic> UserTimeline()
+        {
+            var request = new RestRequest("/1.1/statuses/user_timeline.json");
+            request.RequestFormat = DataFormat.Json;
+            request.AddParameter("user_id", user_id);
+            request.AddParameter("include_rts", false);
+            request.AddParameter("trim_user", true);
+            request.AddParameter("count", 200);
+
+            IRestResponse<dynamic> response = client.Execute<dynamic>(request);
+            logger.Trace("UserTimeline response: {0}", response.Content);
+            return response;
+        }
+
+        public override void Verify()
+        {
+            var response = VerifyCredentials();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                user_id = response.Data.id.Value;
+            }
+            else
+            {
+                logger.Error("Unexpected response to Twitter login: {0}", response.StatusDescription);
+                throw new UnableToVerifyException();
+            }
+        }
+
+        public override bool Check(string triggerString)
+        {
+            if (user_id == -1)
+            {
+                var verifyResponse = VerifyCredentials();
+                if (verifyResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    user_id = verifyResponse.Data.id.Value;
+                }
+                else
+                {
+                    logger.Error("Unexpected response to Twitter login: {0}", verifyResponse.ResponseStatus);
+                    throw new UnableToCheckException();
+                }
+            }
+
+            var tweetsResponse = UserTimeline();
+            var tweets = tweetsResponse.Data.ToObject<List<dynamic>>();
+            foreach (var tweet in tweets)
+            {
+                if (tweet.text.Value.Contains(triggerString))
+                {
+                    logger.Debug("Found trigger string in text: {0}", tweet.Text);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override void Action(List<string> actions)
+        {
+            base.Action(actions);
+        }
+        
     }
 }
