846f553bd98275227b510f0cba4ea4835308612e
UberKitten <566438+UberKitten@users.noreply.github.com>
2018-02-06 03:15:12 -0600

    Implement JFMSUF and main loop checking and actions

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Sentry.Config;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Diagnostics;
 
@@ -19,21 +20,28 @@ namespace Sentry
         protected Dictionary<string, BaseService> services = new Dictionary<string, BaseService>();
         protected Thread loopThread = null;
 
+        protected Dictionary<string, Thread> actionThreads = new Dictionary<string, Thread>();
+        protected Config.Config config = null;
+
         static void Main(string[] args)
         {
             var program = new Program();
 
             var exitEvent = new ManualResetEvent(false);
             Console.CancelKeyPress += (sender, eventArgs) => {
+                program.cancellationToken.Cancel();
                 eventArgs.Cancel = true;
                 exitEvent.Set();
             };
-
-            var options = Parser.Default.ParseArguments<RunOptions, ExampleConfigOptions>(args)
+            var parser = new Parser(with => {
+                with.CaseSensitive = false;
+            });
+            var options = parser.ParseArguments<RunOptions, ExampleConfigOptions>(args)
                 .WithParsed<RunOptions>(opts => program.Run(opts))
                 .WithParsed<ExampleConfigOptions>(opts => program.ExampleConfig(opts));
 
             // Wait here for loop to complete
+            
             exitEvent.WaitOne();
         }
 
@@ -96,7 +104,7 @@ namespace Sentry
 
         public void Run(RunOptions options)
         {
-            var config = GetConfig(options);
+            config = GetConfig(options);
             logger.Info("Loaded {0} with {1} triggers and {2} services", options.ConfigFile, config.Triggers.Count, config.Services.Count);
 
             // Build list of Services
@@ -156,8 +164,138 @@ namespace Sentry
                 }
             }
 
-            // Start loop
-            Loop(options);
+            if (options.JustFuckMyShitUpFam)
+            {
+                if (options.SkipJFMSUFConfirmation)
+                {
+                    logger.Info("Skipping JustFuckMyShitUpFam mode confirmation");
+                }
+                else
+                {
+                    logger.Warn("JustFuckMyShitUpFam mode requested");
+                    Console.WriteLine("**********************************************************");
+                    Console.WriteLine(" WARNING: JustFuckMyShitUpFam mode will run ALL triggers");
+                    Console.WriteLine("    As if ALL trigger phrases had been detected");
+                    Console.WriteLine("      The following actions will be performed:");
+                    Console.WriteLine("");
+
+                    var triggerConfigs = config.Triggers.SelectMany(t => t.Services).ToList();
+                    foreach (var trigger in triggerConfigs)
+                    {
+                        var actionsAggregated = trigger.Actions.Aggregate((sum, addition) => sum + ", " + addition);
+                        Console.WriteLine("  Service: " + trigger.Id + " Actions: " + actionsAggregated);
+                    }
+
+                    Console.WriteLine("");
+                    Console.WriteLine("       These actions may not be reversible");
+                    Console.WriteLine("  There is no additional confirmation or delay");
+                    Console.WriteLine("        Are you SURE you want to do this?");
+                    Console.WriteLine("**********************************************************");
+                    Console.Write("Enter JFMSUF to confirm: ");
+
+                    var userInput = Console.ReadLine();
+                    if (userInput != "JFMSUF")
+                    {
+                        logger.Info("JustFuckMyShitUpFam mode canceled");
+                        Environment.Exit(1);
+                        return;
+                    }
+                }
+                JFMSUF(options);
+            }
+            else
+            {
+                // Start main checking loop
+                Loop(options);
+            }
+
+            // If cancellation is requested via cancellationToken, this exits the main thread
+            Environment.Exit(1);
+        }
+
+        protected void JFMSUF(RunOptions options)
+        {
+            // This is easier to implement here rather than shoe-horning it into the main loop
+            logger.Info("Starting JustFuckMyShitUpFam mode loop");
+
+            if (!options.JustFuckMyShitUpFam)
+            {
+                // Just in case...
+                throw new InvalidOperationException();
+            }
+
+            // Flatten a list of all actions so we can work through them
+            var triggerConfigs = config.Triggers.SelectMany(t => t.Services).ToList();
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var triggerConfigsToRemove = new List<TriggerAction>();
+
+                foreach (var action in triggerConfigs)
+                {
+                    var actionsAggregated = action.Actions.Aggregate((sum, addition) => sum + ", " + addition);
+                    logger.Debug("Attempting to launch {0}, actions {1}", action.Id, actionsAggregated);
+                    
+                    // We want to make sure we don't run multiple actions on the same service at the same time
+                    actionThreads.TryGetValue(action.Id, out Thread currentThread);
+
+                    if (currentThread != null)
+                    {
+                        logger.Trace("Pulled thread id {0} IsAlive {1}", currentThread.ManagedThreadId, currentThread.IsAlive);
+
+                        if (currentThread.IsAlive)
+                        {
+                            // Remove dead thread
+                            logger.Debug("Thread for {0} is finished, removing", action.Id);
+                            actionThreads.Remove(action.Id);
+                        }
+                    }
+
+                    if (currentThread == null || !currentThread.IsAlive)
+                    {
+                        logger.Info("Starting action for {0}, actions {1}", action.Id, actionsAggregated);
+
+                        var serviceToAct = services[action.Id.ToLowerInvariant()];
+                        logger.Trace("Pulled service {0} to act", serviceToAct.GetType());
+
+                        var actionThread = new Thread(() => RunServiceAction(serviceToAct, action.Actions));
+                        actionThreads.Add(action.Id, actionThread);
+                        actionThread.Start();
+
+                        // Remove from list of triggerConfigs to work on so we don't keep trying to start it
+                        // We use triggerConfigsToRemove because we're currently enumerating
+                        triggerConfigsToRemove.Add(action);
+
+                        TraceActionThreads();
+                    }
+                    else
+                    {
+                        logger.Debug("Actions for {0} still running, skipping", action.Id);
+                    }
+                }
+                triggerConfigs.RemoveAll(t => triggerConfigsToRemove.Contains(t));
+
+                TraceActionThreads();
+                var threadsAlive = 0;
+                foreach (var thread in actionThreads)
+                {
+                    if (thread.Value.IsAlive)
+                    {
+                        threadsAlive++;
+                    }
+                }
+                logger.Trace("{0} action threads still alive", threadsAlive);
+
+                if (triggerConfigs.Count <= 0 && threadsAlive <= 0)
+                {
+                    logger.Info("JustFuckMyShitUpFam mode finished, have a nice day!");
+                    cancellationToken.Cancel();
+                }
+                else
+                {
+                    Thread.Sleep(options.JFMSUFLoopDelay * 1000);
+                }
+            }
         }
 
         protected void Loop(RunOptions options)
@@ -165,24 +303,133 @@ namespace Sentry
             logger.Info("Starting main loop");
             while (!cancellationToken.IsCancellationRequested)
             {
-                // Start counting how long it's taken to run checks
-                // So we can check each service roughly every 10 minutes instead of every 10 minutes + time spent checking
-                var timeElapsed = new Stopwatch();
-                timeElapsed.Start();
-
-
-
-                timeElapsed.Stop();
-                logger.Debug("Time taken in loop: {0} ms", timeElapsed.ElapsedMilliseconds);
-
-                // Time left to wait in milliseconds (can be negative)
-                var timeLeft = options.LoopDelay * 1000 - timeElapsed.ElapsedMilliseconds;
-                logger.Trace("Time left to wait: {0} ms", timeLeft);
-
-                if (timeLeft > 0)
+                try
                 {
-                    logger.Trace("Sleeping");
-                    Thread.Sleep(Convert.ToInt32(timeLeft));
+                    // Start counting how long it's taken to run checks
+                    // So we can check each service roughly every 10 minutes instead of every 10 minutes + time spent checking
+                    var timeElapsed = new Stopwatch();
+                    timeElapsed.Start();
+
+                    var i = 0;
+                    foreach (var trigger in config.Triggers)
+                    {
+                        // We don't assign an ID to Triggers, because it's not really necessary so the index in config.Triggers is the de facto ID
+                        logger.Debug("Starting check for trigger index {0}", i);
+
+                        foreach (var triggerString in trigger.TriggerStrings)
+                        {
+                            foreach (var checkId in trigger.Check)
+                            {
+                                logger.Debug("Checking {0} for trigger word {1}", checkId, triggerString);
+
+                                // Pull relevant service to check
+                                var serviceToCheck = services[checkId.ToLowerInvariant()];
+                                logger.Trace("Pulled service to check {0}", serviceToCheck.GetType());
+
+                                // Avoid calling Check if a service is currently doing an Action in another thread
+                                // Simplifies service code to not require thread safety, does reduce concurrency though
+                                // I.e. a service can not be used for checking and acting at the same time (even different triggers)
+                                // The check will wait until the service is done acting
+                                if (actionThreads.ContainsKey(checkId.ToLowerInvariant()))
+                                {
+                                    logger.Info("Skipping check for {0} because actions are currently running", checkId);
+                                }
+                                else
+                                {
+                                    try
+                                    {
+                                        if (serviceToCheck.Check(triggerString))
+                                        {
+                                            logger.Info("Trigger string {0} detected for service {1}", triggerString, checkId);
+
+                                            // Loop through actions and start
+                                            foreach (var action in trigger.Services)
+                                            {
+                                                var actionsAggregated = action.Actions.Aggregate((sum, addition) => sum + ", " + addition);
+                                                logger.Info("Running actions {0} on service {1}", actionsAggregated, action.Id);
+
+                                                var serviceToAct = services[action.Id.ToLowerInvariant()];
+                                                logger.Trace("Pulled service {0} to act", serviceToAct.GetType());
+
+                                                var actionThread = new Thread(() => RunServiceAction(serviceToAct, action.Actions));
+                                                actionThreads.Add(checkId, actionThread);
+                                                actionThread.Start();
+
+                                                TraceActionThreads();
+                                            }
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        logger.Error(ex, "Error while checking {0} for trigger string {1}", checkId, triggerString);
+                                    }
+                                }
+                            }
+                        }
+
+                        i++;
+                    }
+
+                    TraceActionThreads();
+
+                    timeElapsed.Stop();
+                    logger.Debug("Time taken in loop: {0} ms", timeElapsed.ElapsedMilliseconds);
+
+                    // Time left to wait in milliseconds (can be negative)
+                    var timeLeft = options.LoopDelay * 1000 - timeElapsed.ElapsedMilliseconds;
+                    logger.Trace("Time left to wait: {0} ms", timeLeft);
+
+                    if (timeLeft > 0)
+                    {
+                        logger.Trace("Sleeping");
+                        Thread.Sleep(Convert.ToInt32(timeLeft));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // This should only trigger on a problem with Sentry's code itself
+                    logger.Error(ex, "Error during main loop");
+
+                    // Ideally we would exit or do some kind of action or cleanup here,
+                    // but the show (and actions + checks) must go on
+                }
+            }
+        }
+
+        protected void RunServiceAction(BaseService service, List<string> actions)
+        {
+            // TODO: Quorum + clustering
+            // TODO: Keep trying during exception
+            try
+            {
+                service.Action(actions);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error while running action");
+            }
+        }
+
+        /**
+         * Print trace information on actionThreads
+         * Ideally should be run at the end of loops and after adding/removing to actionThreads
+         */ 
+        private void TraceActionThreads()
+        {
+            if (actionThreads.Count > 0)
+            {
+                logger.Trace("TraceActionThreads:");
+                foreach (var thread in actionThreads)
+                {
+                    if (thread.Value == null)
+                    {
+                        // Shouldn't happen
+                        logger.Warn("Null thread for {0} in actionThreads", thread.Key);
+                    }
+                    else
+                    {
+                        logger.Trace("ActionThread {0} Id {1} IsAlive {2} ThreadState {3}", thread.Key, thread.Value.ManagedThreadId, thread.Value.IsAlive, thread.Value.ThreadState);
+                    }
                 }
             }
         }
